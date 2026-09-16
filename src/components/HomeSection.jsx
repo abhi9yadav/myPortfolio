e4cb66d072d15f2f514deddf4ddb64d0ff31c927
abhi9yadav <acbabhinav854@gmail.com>
2026-09-16 14:08:43 +0530

    add resume

--- src/components/HomeSection.jsx
+++ src/components/HomeSection.jsx
@@ -1,10 +1,11 @@
 // src/components/HomeSection.jsx
 import React from 'react';
 import { Rocket, Terminal, Code2, Cpu } from 'lucide-react';
+const heroImage="../assets/banner.png";
 
 const HomeSection = ({ sectionRef, isVisible, currentTheme, scrollToSection, handleMouseEnterInteractive, handleMouseLeaveInteractive, useParticles }) => {
   const canvasRef = useParticles();
-  const heroImage = `${import.meta.env.BASE_URL}ChatGPT%20Image%20Sep%2015,%202026,%2010_05_09%20PM.png`;
+  // const heroImage = `${import.meta.env.BASE_URL}ChatGPT%20Image%20Sep%2015,%202026,%2010_05_09%20PM.png`;
 
   return (
     <section id="home" ref={sectionRef} className={`relative flex min-h-screen items-center justify-center ${currentTheme.homeSectionBg} ${currentTheme.textDefault} p-6 md:p-12 overflow-hidden border-b ${currentTheme.navBorder}`}>
